f41995321c185e537725689dde10fe117266565d
ravi25595 <ravi25595@gmail.com>
2025-09-10 21:51:22 +0530

    docs

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { RouterLink } from "../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
 @Component({
   selector: 'app-home-page',
-  imports: [MatIcon, MatSlideToggle, FormsModule],
+  imports: [MatIcon, MatSlideToggle, FormsModule, MatButtonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
